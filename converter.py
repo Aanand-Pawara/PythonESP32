b484727aaa9a98f902a6e541d0d6dd63c5e3cdfa
MNDRiN <75675267+Aanand-Pawara@users.noreply.github.com>
2025-09-15 08:02:20 +0530

    init 0.1

--- converter.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load the model
-model = YOLO("best.pt")
-
-# Export to ONNX (make sure ONNX installed)
-model.export(format="onnx", opset=12, dynamic=False, simplify=True, imgsz=640)
-
-print("✅ Exported to ONNX.")
